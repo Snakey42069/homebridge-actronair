ee6ec69224e9c26ae37035a43d4c7d2e4d2efdfd
Snakey42069 <67512936+Snakey42069@users.noreply.github.com>
2022-03-09 19:07:17 +1100

    maybe v 3

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,4 +1,4 @@
-import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
+import { Service, PlatformAccessory, CharacteristicValue, Characteristic } from 'homebridge';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const request = require('request');
 import { ExampleHomebridgePlatform } from './platform';
@@ -10,15 +10,7 @@ import { ExampleHomebridgePlatform } from './platform';
  */
 export class ExamplePlatformAccessory {
   private service: Service;
-
-  /**
-   * These are just used to create a working example
-   * You should implement your own code to track the state of your accessory
-   */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
-  };
+  // private last_state: {amOn: boolean; tempTarget: number; mode: number; fanSpeed: number; enabledZones: number[]} = {};
 
   constructor(
     private readonly platform: ExampleHomebridgePlatform,
@@ -27,18 +19,14 @@ export class ExamplePlatformAccessory {
     // set accessory information
     this.accessory
       .getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'ActronAir')
-      .setCharacteristic(this.platform.Characteristic.Model, 'ESP-Ultima')
-      .setCharacteristic(
-        this.platform.Characteristic.SerialNumber,
-        '123-456-789',
-      );
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'ActronAir');
+
 
     // get the LightBulb service if it exists, otherwise create a new LightBulb service
     // you can create multiple services for each accessory
     this.service =
-      this.accessory.getService(this.platform.Service.Switch) ||
-      this.accessory.addService(this.platform.Service.Switch);
+      this.accessory.getService(this.platform.Service.HeaterCooler) ||
+      this.accessory.addService(this.platform.Service.HeaterCooler);
 
     // set the service name, this is what is displayed as the default name on the Home app
     // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
@@ -47,66 +35,40 @@ export class ExamplePlatformAccessory {
       accessory.context.device.name,
     );
 
+    this.service.setCharacteristic(
+      this.platform.Characteristic.TemperatureDisplayUnits,
+      this.platform.Characteristic.TemperatureDisplayUnits.CELSIUS,
+    );
+
     // each service must implement at-minimum the "required characteristics" for the given service type
     // see https://developers.homebridge.io/#/service/Lightbulb
 
     // register handlers for the On/Off Characteristic
     this.service
-      .getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this)) // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this)); // GET - bind to the `getOn` method below
+      .getCharacteristic(this.platform.Characteristic.Active)
+      .onSet(this.setActiveHandler.bind(this))
+      .onGet(this.getActiveHandler.bind(this));
 
-    /**
-     * Creating multiple services of the same type.
-     *
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     *
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
+    this.service
+      .getCharacteristic(this.platform.Characteristic.CurrentHeaterCoolerState)
+      .onGet(this.getCurrentHeaterCoolerStateHandler.bind(this)); // GET - bind to the `getOn` method below
 
-    /**
-     * Updating characteristics values asynchronously.
-     *
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     *
-     */
+    this.service.getCharacteristic(this.platform.Characteristic.CurrentHeaterCoolerState).setProps({maxValue: 4});
+
+    this.service.getCharacteristic(this.platform.Characteristic.TargetHeaterCoolerState)
+      .onGet(this.handleTargetHeaterCoolerStateGet.bind(this))
+      .onSet(this.handleTargetHeaterCoolerStateSet.bind(this));
+
+    this.service.getCharacteristic(this.platform.Characteristic.CurrentTemperature)
+      .onGet(this.handleTargetHeaterCoolerStateGet.bind(this));
+
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
-  async setOn(value: CharacteristicValue) {
-    // implement your own code to turn your device on/off
-    // try {
-    //   const { data } = await got
-    //     .post(`http://${this.accessory.context.device.ip}`, {
-    //       method: 'GET',
-    //       timeout: { request: 5000 },
-    //       json: {
-    //         DA: { amOn: !!value },
-    //       },
-    //     })
-    //     .json();
-    //   this.platform.log.debug('Data recieved from actron POST req ->', data);
-    //   this.platform.log.debug('Get Characteristic On ->', value);
-    // } catch (error) {
-    //   this.platform.log.debug('Actron Error in SET->', error);
-    //   throw new this.platform.api.hap.HapStatusError(
-    //     this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE,
-    //   );
-    // }
+  setActiveHandler(value: CharacteristicValue) {
     // eslint-disable-next-line max-len
     const url = `https://que.actronair.com.au/rest/v0/device/${this.accessory.context.device.device_token}?user_access_token=${this.accessory.context.device.user_token}`;
-    this.platform.log.info('ActronURL ->', url);
+    this.platform.log.debug('ActronURL ->', url);
     request({
-
       url: url,
       body: JSON.stringify({'DA':{'amOn': !!value}}),
       method: 'PUT',
@@ -115,34 +77,20 @@ export class ExamplePlatformAccessory {
       qs: {'user_access_token': this.accessory.context.device.user_token},
     }, (error, response, body) => {
       if (error) {
-        this.platform.log.info('Actron Error in SET->', error);
+        this.platform.log.debug('Actron Error in SET->', error);
         throw new this.platform.api.hap.HapStatusError(
           this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE,
         );
       } else {
-        this.platform.log.info('Data recieved from actron POST req ->', body);
-        this.platform.log.info('Get Characteristic On pre POST ->', value);
+        this.platform.log.debug('Data recieved from actron POST req ->', body);
+        this.platform.log.info('Set aircon state to ->', value);
       }
     });
 
   }
 
-  /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   *
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   *
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
-
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
-  async getOn(): Promise<CharacteristicValue> {
+  async getActiveHandler(): Promise<CharacteristicValue> {
     return new Promise<CharacteristicValue>((resolve, reject) => {
-
       // eslint-disable-next-line max-len
       const url = `https://que.actronair.com.au/rest/v0/device/${this.accessory.context.device.device_token}?user_access_token=${this.accessory.context.device.user_token}`;
       request({
@@ -162,20 +110,158 @@ export class ExamplePlatformAccessory {
         } else {
           b = body;
         }
-        this.platform.log.info('Data recieved from actron GET req ->', b);
-        this.platform.log.info('Get Characteristic On ->', !!b.data.last_data.DA.amOn);
+        this.platform.log.debug('Data recieved from actron GET req ->', b);
+        this.platform.log.info('Aircon state ->', !!b.data.last_data.DA.amOn);
         resolve(!!b.data.last_data.DA.amOn as CharacteristicValue);
       });
 
     });
-
-
-    // if you need to return an error to show the device as "Not Responding" in the Home app:
-    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-   */
+  async getCurrentHeaterCoolerStateHandler(): Promise<CharacteristicValue> {
+    return new Promise<CharacteristicValue>((resolve, reject) => {
+      // eslint-disable-next-line max-len
+      const url = `https://que.actronair.com.au/rest/v0/device/${this.accessory.context.device.device_token}?user_access_token=${this.accessory.context.device.user_token}`;
+      request({
+        url: url,
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+        timeout: 5000,
+      }, (error, response, body) => {
+        if (error) {
+          this.platform.log.debug('Actron Error in GET->', error);
+          reject(new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE));
+        }
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        let b: Record<any, any>;
+        if (typeof body === 'string') {
+          b = JSON.parse(body);
+        } else {
+          b = body;
+        }
+        const m = b.data.last_data.DA.mode;
+        this.platform.log.debug('Data recieved from actron currentHeaterCool GET req ->', b);
+        if (!b.data.last_data.DA.amOn) {
+          this.platform.log.info('Aircon state ->', 'INACTIVE');
+          resolve(this.platform.Characteristic.CurrentHeaterCoolerState.INACTIVE);
+        }else if(m===0) {
+          this.platform.log.info('Aircon state ->', 'AUTO');
+          resolve(4);
+        } else if (m === 1) {
+          this.platform.log.info('Aircon state ->', 'HEATING');
+          resolve(this.platform.Characteristic.CurrentHeaterCoolerState.HEATING);
+        } else if (m ===2) {
+          this.platform.log.info('Aircon state ->', 'COOLING');
+          resolve(this.platform.Characteristic.CurrentHeaterCoolerState.COOLING);
+        } else {
+          this.platform.log.info('Aircon state ->', 'FAN/IDLE');
+          resolve(this.platform.Characteristic.CurrentHeaterCoolerState.IDLE);
+        }
+      });
+    });
+  }
+
+  async handleTargetHeaterCoolerStateGet():Promise<CharacteristicValue> {
+    return new Promise<CharacteristicValue>((resolve, reject) => {
+      // eslint-disable-next-line max-len
+      const url = `https://que.actronair.com.au/rest/v0/device/${this.accessory.context.device.device_token}?user_access_token=${this.accessory.context.device.user_token}`;
+      request({
+        url: url,
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+        timeout: 5000,
+      }, (error, response, body) => {
+        if (error) {
+          this.platform.log.debug('Actron Error in GET->', error);
+          reject(new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE));
+        }
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        let b: Record<any, any>;
+        if (typeof body === 'string') {
+          b = JSON.parse(body);
+        } else {
+          b = body;
+        }
+        const m = b.data.last_data.DA.mode;
+        this.platform.log.debug('Data recieved from actron currentHeaterCool GET req ->', b);
+        this.platform.log.debug('Get CurrentHeaterCoolerState Characteristic ->', m);
+        if(m===0) {
+          this.platform.log.info('Aircon target state ->', 'AUTO');
+          resolve(this.platform.Characteristic.TargetHeaterCoolerState.AUTO);
+        } else if (m === 1) {
+          this.platform.log.info('Aircon target state ->', 'HEAT');
+          resolve(this.platform.Characteristic.TargetHeaterCoolerState.HEAT);
+        } else if (m ===2) {
+          this.platform.log.info('Aircon target state ->', 'COOL');
+          resolve(this.platform.Characteristic.TargetHeaterCoolerState.COOL);
+        } else {
+          this.platform.log.info('Aircon target state ->', 'COOL/FAN');
+          resolve(this.platform.Characteristic.TargetHeaterCoolerState.COOL);
+        }
+      });
+    });
+  }
+
+  handleTargetHeaterCoolerStateSet(value) {
+    // eslint-disable-next-line max-len
+    const url = `https://que.actronair.com.au/rest/v0/device/${this.accessory.context.device.device_token}?user_access_token=${this.accessory.context.device.user_token}`;
+    this.platform.log.debug('ActronURL ->', url);
+    let d = value;
+    if (value === this.platform.Characteristic.TargetHeaterCoolerState.AUTO ) {
+      d = 0;
+    } else if ( value === this.platform.Characteristic.TargetHeaterCoolerState.HEAT) {
+      d = 1;
+    } else if (value === this.platform.Characteristic.TargetHeaterCoolerState.COOL) {
+      d = 2;
+    } else {
+      d = 3;
+    }
+    request({
+      url: url,
+      body: JSON.stringify({'DA':{'mode': d}}),
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      timeout: 5000,
+      qs: {'user_access_token': this.accessory.context.device.user_token},
+    }, (error, response, body) => {
+      if (error) {
+        this.platform.log.debug('Actron Error in SET->', error);
+        throw new this.platform.api.hap.HapStatusError(
+          this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE,
+        );
+      } else {
+        this.platform.log.debug('Data recieved from actron POST req ->', body);
+        this.platform.log.debug('Aircon target state set to ->', value);
+      }
+    });
+  }
+
+  handleCurrentTemperatureGet() {
+    return new Promise<CharacteristicValue>((resolve, reject) => {
+      // eslint-disable-next-line max-len
+      const url = `http://${this.accessory.context.device.ip}/6.json`;
+      request({
+        url: url,
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+        timeout: 5000,
+      }, (error, response, body) => {
+        if (error) {
+          this.platform.log.debug('Actron Error in GET->', error);
+          reject(new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE));
+        }
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        let b: Record<any, any>;
+        if (typeof body === 'string') {
+          b = JSON.parse(body);
+        } else {
+          b = body;
+        }
+        this.platform.log.debug('Data recieved from actron GET req ->', b);
+        this.platform.log.info('Aircon current temp ->', b.data.last_data.DA.roomTemp_oC);
+        resolve(b.data.last_data.DA.roomTemp_oC as CharacteristicValue);
+      });
+
+    });
+  }
 }
