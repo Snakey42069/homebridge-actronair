000232903c833723b10259bfaeefd28fd3eba44f
Snakey42069 <67512936+Snakey42069@users.noreply.github.com>
2022-03-15 13:54:40 +1100

    enabled hot reload of config

--- src/platform.ts
+++ src/platform.ts
@@ -94,8 +94,8 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
         );
 
         // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-        // existingAccessory.context.device = device;
-        // this.api.updatePlatformAccessories([existingAccessory]);
+        existingAccessory.context.device = device;
+        this.api.updatePlatformAccessories([existingAccessory]);
 
         // create the accessory handler for the restored accessory
         // this is imported from `platformAccessory.ts`
